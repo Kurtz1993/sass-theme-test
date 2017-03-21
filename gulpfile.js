4b54d43b5c92acfcbc7cd5dbdfb3fe2f5e913887
Luis Rogelio Hernández López <Kurtz@Luiss-MacBook-Pro.local>
2017-03-21 12:49:03 -0600

    Add themes as entry points and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', () => {
-    return gulp.src(['./src/themes/*.scss', './src/default.scss'])
-        .pipe(sass())
+    return gulp.src(['./src/themes/*.scss'])
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
         .pipe(gulp.dest('dist'));
 });
